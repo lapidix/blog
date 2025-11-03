e5262014a79a5c93d2da4e89b8e6555e5afa706b
이민기 <mingi3442@gmail.com>
2025-11-03 19:24:56 +0900

    ♻️ Remove unused Sentry error tracking from Image component

--- components/common/atoms/Image.tsx
+++ components/common/atoms/Image.tsx
@@ -24,25 +24,6 @@ const Image = ({
       sizes={rest.sizes || '(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'}
       {...rest}
       {...(useFill && { fill: true, style: { objectFit: 'cover', ...rest.style } })}
-      onError={(e) => {
-        const error = new Error('Image loading failed')
-        const { captureException } = require('@sentry/nextjs')
-
-        captureException(error, {
-          fingerprint: ['image-loading-failed'],
-          tags: {
-            error_type: 'image_loading',
-            component: 'Image',
-          },
-          contexts: {
-            image: {
-              src: typeof src === 'string' ? src : 'object',
-              alt,
-            },
-          },
-          level: 'warning',
-        })
-      }}
     />
   )
 }
