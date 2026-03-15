26ac371185c7f9e1a28ce46a37a5f2c3ac729ae1
이민기 <mingi3442@gmail.com>
2026-03-15 16:27:15 +0900

    🚧 Post on Frontend Clean Architecture (FSD & DDD) detailing the integration of architectural patterns, including images and core concepts.

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -90,7 +90,7 @@ export default async function PostLayout({
                   <dt className="sr-only">Published on</dt>
                   <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
                     <time dateTime={date}>
-                      {new Date(date).toLocaleDateString(siteMetadata.locale, postDateTemplate)}
+                      {new Date(date).toLocaleDateString('en-US', postDateTemplate)}
                     </time>
                   </dd>
                 </div>
