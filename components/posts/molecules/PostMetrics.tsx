71c6ea688193223182a42fb809129f8cc5176097
이민기 <mingi3442@gmail.com>
2026-03-21 17:40:06 +0900

    💄 Update PostMetrics component styling to improve layout and responsiveness, ensuring translation link is centered and visually appealing.

--- components/posts/molecules/PostMetrics.tsx
+++ components/posts/molecules/PostMetrics.tsx
@@ -22,13 +22,13 @@ export default function PostMetrics({
   ...props
 }: PostMetricProps) {
   return (
-    <div className={ctm(className)} {...props}>
+    <div className={ctm('flex-wrap', className)} {...props}>
       <ReadingTime readingTime={readingTime} />
       <ViewCounter viewCount={viewCount} />
       {translationExists && translationLink && (
         <Link
           href={translationLink}
-          className="text-sm font-medium leading-6 text-gray-500 dark:text-gray-400 hover:text-primary-500 dark:hover:text-primary-400 transition-colors flex items-center gap-2 rounded-md hover:bg-gray-100 dark:hover:bg-gray-800 p-1 -ml-1 active:scale-95"
+          className="basis-full justify-center text-sm font-medium leading-6 text-gray-500 dark:text-gray-400 hover:text-primary-500 dark:hover:text-primary-400 transition-colors flex items-center gap-2 rounded-md hover:bg-gray-100 dark:hover:bg-gray-800 p-1 active:scale-95"
         >
           <TranslateIcon className="size-4" />
           {locale === 'en' ? 'Read in Korean' : 'Read in English'}
